cb75d08b6210a21acb046cdf2991739e61a23249
Martin Walker <martin.walker@contentful.com>
2018-10-22 22:14:18 +0200

    Built entry animation, added about page, and added projects page

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,3 @@
-//page transitions
-(function() {
-  const wrapper = document.querySelector('.content-wrapper');
-
-  function transitionPage () {
-    wrapper.classList.add('slide-in')
-  }
-
-  window.addEventListener('load', transitionPage);
-})();
-
 //hover effect til-index
 (function(){
   const categoryTitles = document.querySelectorAll('.category__title');
@@ -19,4 +8,32 @@
 
   categoryTitles.forEach(title => title.addEventListener('mouseover', addHoverEffect));
   categoryTitles.forEach(title => title.addEventListener('mouseout', addHoverEffect));
+})();
+
+//smiley animation on home page
+(function() {
+  const hero = document.querySelector('.hero');
+
+  console.log(window.sessionStorage);
+  function activateAnimations () {
+    //return if not homepage
+    if (hero === null) {
+      return;
+    }
+    //return if they've already seen the animation
+    if (window.sessionStorage.homeVisit === "true") {
+      hero.classList.remove('hero-animate');
+      return;
+    }
+    let navigation = document.querySelector('.navigation');
+    hero.classList.add('active')
+    navigation.classList.add('home-navigation')
+    setTimeout(() => {
+      hero.classList.remove('active', 'hero-animate')
+      navigation.classList.remove('home-navigation')
+    }, 3700)
+    window.sessionStorage.setItem('homeVisit', true);
+  }
+
+  window.addEventListener('load', activateAnimations);
 })();
