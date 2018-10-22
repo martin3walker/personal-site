b05f5c1e2e41ecfe257514a4c64e5db1b0d89600
Martin Walker <martin.walker@contentful.com>
2018-10-22 23:27:34 +0200

    Made navigation change background-color on home page

--- assets/js/index.js
+++ assets/js/index.js
@@ -14,7 +14,6 @@
 (function() {
   const hero = document.querySelector('.hero');
 
-  console.log(window.sessionStorage);
   function activateAnimations () {
     //return if not homepage
     if (hero === null) {
@@ -25,15 +24,43 @@
       hero.classList.remove('hero-animate');
       return;
     }
-    let navigation = document.querySelector('.navigation');
+    let navigation = document.querySelector('.navigation')
     hero.classList.add('active')
-    navigation.classList.add('home-navigation')
+    navigation.classList.add('home-navigation', 'navigation-transparent')
     setTimeout(() => {
       hero.classList.remove('active', 'hero-animate')
       navigation.classList.remove('home-navigation')
     }, 3700)
-    window.sessionStorage.setItem('homeVisit', true);
+    window.sessionStorage.setItem('homeVisit', true)
   }
 
   window.addEventListener('load', activateAnimations);
+})();
+
+//Navigation transition on home page
+(function() {
+  let navigation = document.querySelector('.hero-navigation');
+  if (navigation === null) {
+    return;
+  }
+  let hero = document.querySelector('.hero');
+  let options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: .1
+  }
+
+  let observer = new IntersectionObserver(makeVisible, options);
+  function makeVisible(event) {
+    console.log(event[0])
+    if(event[0].intersectionRatio > .5){
+      return;
+    }
+    else {
+      navigation.classList.toggle('hero-navigation');
+    }
+
+  }
+
+  observer.observe(hero);
 })();
