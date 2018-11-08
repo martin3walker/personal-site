1ec1e598dcbc33dbaeebf8bd6e73154b4fc42604
Martin Walker <martin.walker@contentful.com>
2018-11-08 16:17:38 +0100

    :fire: Removed homepage animation because it is poor UX

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,32 +10,6 @@
   categoryTitles.forEach(title => title.addEventListener('mouseout', addHoverEffect));
 })();
 
-//smiley animation on home page
-(function() {
-  const hero = document.querySelector('.hero');
-
-  function activateAnimations () {
-    //return if not homepage
-    if (hero === null) {
-      return;
-    }
-    //return if they've already seen the animation
-    if (window.sessionStorage.homeVisit === "true") {
-      hero.classList.remove('hero-animate');
-      return;
-    }
-    let navigation = document.querySelector('.navigation')
-    hero.classList.add('active')
-    navigation.classList.add('home-navigation', 'navigation-transparent')
-    setTimeout(() => {
-      hero.classList.remove('active', 'hero-animate')
-      navigation.classList.remove('home-navigation')
-    }, 3700)
-    window.sessionStorage.setItem('homeVisit', true)
-  }
-
-  window.addEventListener('load', activateAnimations);
-})();
 
 //Navigation transition on home page
 (function() {
