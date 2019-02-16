eee4422b3f64e42c41c97d122ac278e6aa538f96
Martin Walker <martin.walker@contentful.com>
2019-02-16 12:04:24 +0100

    :art: Added a pollyfill for intersection observer set on my header

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,50 +1,84 @@
+const polyfill = require('dynamic-polyfill');
+
 //hover effect til-index
-(function(){
+(function() {
   const categoryTitles = document.querySelectorAll('.category__title');
 
-  function addHoverEffect (event) {
-    event.target.parentNode.parentNode.parentNode.classList.toggle('active-hover');
+  function addHoverEffect(event) {
+    event.target.parentNode.parentNode.parentNode.classList.toggle(
+      'active-hover'
+    );
   }
 
-  categoryTitles.forEach(title => title.addEventListener('mouseover', addHoverEffect));
-  categoryTitles.forEach(title => title.addEventListener('mouseout', addHoverEffect));
+  categoryTitles.forEach(title =>
+    title.addEventListener('mouseover', addHoverEffect)
+  );
+  categoryTitles.forEach(title =>
+    title.addEventListener('mouseout', addHoverEffect)
+  );
 })();
-
 
 //Navigation transition on home page
-(function() {
-  let navigation = document.querySelector('.hero-navigation');
-  if (navigation === null) {
-    return;
-  }
-  let hero = document.querySelector('.hero');
-  let options = {
-    root: null,
-    rootMargin: '0px',
-    threshold: .1
-  }
-
-  let observer = new IntersectionObserver(makeVisible, options);
-  function makeVisible(event) {
-    if(event[0].intersectionRatio > .5){
+polyfill({
+  fills: ['IntersectionObserver'],
+  minify: false,
+  rum: false,
+  afterFill() {
+    let navigation = document.querySelector('.hero-navigation');
+    if (navigation === null) {
       return;
     }
-    else {
-      navigation.classList.toggle('hero-navigation');
+    let hero = document.querySelector('.hero');
+    let options = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0.1
+    };
+
+    let observer = new IntersectionObserver(makeVisible, options);
+    function makeVisible(event) {
+      if (event[0].intersectionRatio > 0.5) {
+        return;
+      } else {
+        navigation.classList.toggle('hero-navigation');
+      }
     }
 
+    observer.observe(hero);
   }
+})(
+  // (function() {
+  //   let navigation = document.querySelector('.hero-navigation');
+  //   if (navigation === null) {
+  //     return;
+  //   }
+  //   let hero = document.querySelector('.hero');
+  //   let options = {
+  //     root: null,
+  //     rootMargin: '0px',
+  //     threshold: 0.1
+  //   };
+  //
+  //   let observer = new IntersectionObserver(makeVisible, options);
+  //   function makeVisible(event) {
+  //     if (event[0].intersectionRatio > 0.5) {
+  //       return;
+  //     } else {
+  //       navigation.classList.toggle('hero-navigation');
+  //     }
+  //   }
+  //
+  //   observer.observe(hero);
+  // })();
 
-  observer.observe(hero);
-})();
+  function() {
+    const navigation = document.querySelector('.navigation');
+    const controls = document.querySelector('.navigation__controls');
 
-(function() {
-  const navigation = document.querySelector('.navigation');
-  const controls = document.querySelector('.navigation__controls');
+    function activateMenu() {
+      navigation.classList.toggle('active-menu');
+    }
 
-  function activateMenu () {
-    navigation.classList.toggle('active-menu');
+    controls.addEventListener('click', activateMenu);
   }
-
-  controls.addEventListener('click', activateMenu);
-})();
+)();
